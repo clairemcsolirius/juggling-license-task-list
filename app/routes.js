a98a72061d71b2be0dfc35d8d3c72f9ec61e3db9
Claire <claire.mcshane@solirius.com>
2025-06-10 21:41:14 +0100

    add personal details flow

--- app/routes.js
+++ app/routes.js
@@ -38,3 +38,4 @@ router.post('/has-account',(req, res) => {
   })
 
 require('./routes/application/show')(router)
+require('./routes/application/edit-personal-details')(router)
---
+++ app/routes/application/edit-personal-details.js
@@ -0,0 +1,14 @@
+module.exports = router =>{
+
+router.post('/application/edit-personal-details/name', (req, res) => {
+    res.redirect('telephone')
+})
+
+router.post('/application/edit-personal-details/telephone', (req, res) => {
+    res.redirect('address')
+})
+
+router.post('/application/edit-personal-details/address', (req, res) => {
+    res.redirect('check')
+})
+}
